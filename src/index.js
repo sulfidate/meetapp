24ec112fd06088c711d7704f83c6084c3ac6e483
sulfidate <ffluid@me.com>
2022-05-09 18:55:04 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
+// import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
@@ -7,12 +8,16 @@ import reportWebVitals from './reportWebVitals'
 import * as atatus from 'atatus-spa'
 atatus.config('bd3637eb11534fb6818085221cf2ba9e').install()
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
