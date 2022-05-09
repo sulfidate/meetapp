329bfc90cc3c4b4cc74a48704b100efebd8ce29d
sulfidate <ffluid@me.com>
2022-05-09 09:55:21 +0200

    update

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -8,10 +8,10 @@ function WelcomeScreen(props) {
         Log in to see upcoming events around the world for full-stack developers
       </h4>
       <div className='button_cont' align='center'>
-        <div class='google-btn'>
-          <div class='google-icon-wrapper'>
+        <div className='google-btn'>
+          <div className='google-icon-wrapper'>
             <img
-              class='google-icon'
+              className='google-icon'
               src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
               alt='Google sign-in'
             />
@@ -21,7 +21,7 @@ function WelcomeScreen(props) {
               props.getAccessToken()
             }}
             rel='nofollow noopener'
-            class='btn-text'
+            className='btn-text'
           >
             <b>Sign in with google</b>
           </button>
