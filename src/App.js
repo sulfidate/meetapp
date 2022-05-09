2d778e2bd896de3472f886ec74d610ed1c967443
sulfidate <ffluid@me.com>
2022-05-09 16:14:53 +0200

    last changes

--- src/App.js
+++ src/App.js
@@ -17,42 +17,34 @@ class App extends Component {
     showWelcomeScreen: undefined,
   }
 
-  // async componentDidMount() {
-  //   this.mounted = true
-  //   const accessToken = localStorage.getItem('access_token')
-  //   const isTokenValid = (await checkToken(accessToken)).error ? false : true
-  //   const searchParams = new URLSearchParams(window.location.search)
-  //   const code = searchParams.get('code')
-  //   this.setState({ showWelcomeScreen: !(code || isTokenValid) })
-  //   if ((code || isTokenValid) && this.mounted) {
-  //     getEvents().then((events) => {
-  //       if (this.mounted) {
-  //         this.setState({ events, locations: extractLocations(events) })
-  //       }
-  //     })
-  //   }
-  // }
-
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({
-          events: events.slice(0, this.state.numberOfEvents),
-          locations: extractLocations(events),
-        })
-      }
-      if (!navigator.onLine) {
-        this.setState({
-          OfflineAlertText:
-            'There is no internet connection - event-list is loading from cache!',
-        })
-      } else {
-        this.setState({
-          OfflineAlertText: '',
-        })
-      }
-    })
+
+    const accessToken = localStorage.getItem('access_token')
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true
+    const searchParams = new URLSearchParams(window.location.search)
+    const code = searchParams.get('code')
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) })
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events),
+          })
+        }
+        if (!navigator.onLine) {
+          this.setState({
+            OfflineAlertText:
+              'There is no internet connection - event-list is loading from cache!',
+          })
+        } else {
+          this.setState({
+            OfflineAlertText: '',
+          })
+        }
+      })
+    }
   }
 
   componentWillUnmount() {
@@ -88,8 +80,8 @@ class App extends Component {
   }
 
   render() {
-    // if (this.state.showWelcomeScreen === undefined)
-    //   return <div className='App' />
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className='App' />
 
     const { events, locations, numberOfEvents, OfflineAlertText } = this.state
 
