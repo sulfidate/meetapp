73847abb64cfcf7ea89e67e1b639e944e3441e88
sulfidate <ffluid@me.com>
2022-05-09 21:32:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,75 +19,58 @@ class App extends Component {
 
   async componentDidMount() {
     this.mounted = true
-
     const accessToken = localStorage.getItem('access_token')
     const isTokenValid = (await checkToken(accessToken)).error ? false : true
     const searchParams = new URLSearchParams(window.location.search)
     const code = searchParams.get('code')
     this.setState({ showWelcomeScreen: !(code || isTokenValid) })
-
     if ((code || isTokenValid) && this.mounted) {
       getEvents().then((events) => {
-        this.setState({
-          events: events.slice(0, this.state.numberOfEvents),
-          locations: extractLocations(events),
-        })
-      })
-    }
-  }
-
-  // if (!navigator.onLine) {
-  //   this.setState({
-  //     OfflineAlertText:
-  //       'There is no internet connection - event-list is loading from cache!',
-  //   })
-  // } else {
-  //   this.setState({
-  //     OfflineAlertText: '',
-  //   })
-  // }
-
-  componentWillUnmount() {
-    this.mounted = false
-    this.setState = (state, callback) => {
-      return
-    }
-  }
-
-  updateEvents = (location = 'all', number = this.state.numberOfEvents) => {
-    if (navigator.onLine) {
-      this.setState({
-        infoText: '',
-      })
-      getEvents().then((events) => {
-        const locationEvents =
-          location === 'all'
-            ? events.slice(0, number)
-            : events
-                .filter((event) => event.location === location)
-                .slice(0, number)
         if (this.mounted) {
           this.setState({
-            events: locationEvents.slice(0, number),
-            location,
-          })
-        } else {
-          this.setState({
-            infoText:
-              'There is no internet connection - event-list is loading from cache!',
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events),
           })
         }
       })
     }
   }
 
+  componentWillUnmount() {
+    this.mounted = false
+  }
+
   updateNumberOfEvents = (numberOfEvents) => {
-    this.setState(
-      {
-        numberOfEvents,
-      },
-      this.updateEvents(this.state.location, numberOfEvents)
-    )
+    this.setState({
+      numberOfEvents,
+    })
+  }
+
+  updateEvents = (location = 'all', number = this.state.numberOfEvents) => {
+    getEvents().then((events) => {
+      const locationEvents =
+        location === 'all'
+          ? events.slice(0, number)
+          : events
+              .filter((event) => event.location === location)
+              .slice(0, number)
+      if (this.mounted) {
+        this.setState({
+          events: locationEvents.slice(0, number),
+          location,
+        })
+        if (!navigator.onLine) {
+          this.setState({
+            infoText:
+              'There is no internet connection - event-list is loading from cache!',
+          })
+        } else {
+          this.setState({
+            infoText: '',
+          })
+        }
+      }
+    })
   }
 
   render() {
@@ -98,7 +81,8 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <OfflineAlert text={this.state.infoText} />
+        {/* <OfflineAlert text={this.state.infoText} /> */}
+
         <CitySearch
           locations={locations}
           numberOfEvents={numberOfEvents}
@@ -117,7 +101,8 @@ class App extends Component {
           numberOfEvents={numberOfEvents}
           updateEvents={this.updateEvents}
         />
-        <OfflineAlert text={OfflineAlertText} />
+
+        {/* <OfflineAlert text={OfflineAlertText} /> */}
 
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
