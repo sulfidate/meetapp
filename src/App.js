a1acbb3d8b022d1ff3515d59e77509a2a474afd5
sulfidate <ffluid@me.com>
2022-05-10 17:35:40 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import NumberOfEvents from './NumberOfEvents'
 import WelcomeScreen from './WelcomeScreen'
 import { getEvents, extractLocations, checkToken, getAccessToken } from './api'
 import { OfflineAlert } from './Alert'
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts'
 
 class App extends Component {
   state = {
@@ -93,6 +102,18 @@ class App extends Component {
     this.mounted = false
   }
 
+  getData = () => {
+    const { locations, events } = this.state
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length
+      const city = location.split(', ').shift()
+      return { city, number }
+    })
+    return data
+  }
+
   render() {
     const { events, locations, offlineText, showWelcomeScreen } = this.state
 
@@ -111,6 +132,29 @@ class App extends Component {
           </div>
 
           <OfflineAlert text={offlineText} />
+
+          <h4>Events in each city</h4>
+          <ResponsiveContainer height={400}>
+            <ScatterChart
+              margin={{
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+              }}
+            >
+              <CartesianGrid />
+              <XAxis type='category' dataKey='city' name='city' />
+              <YAxis
+                type='number'
+                dataKey='number'
+                name='number of events'
+                allowDecimals={false}
+              />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill='#8884d8' />
+            </ScatterChart>
+          </ResponsiveContainer>
 
           <EventList events={events} />
           <OfflineAlert text={this.state.OfflineAlertText} />
